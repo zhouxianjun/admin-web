3ed376591fc92078aa16b990c6f19866e5f03b8d
zhouxianjun <gary@cn-face.com>
2016-07-21 17:41:36 +0800

    菜单操作

--- src/MQTTServer.js
+++ src/MQTTServer.js
@@ -2,15 +2,20 @@
  * Created by cz on 2016/7/21.
  */
 const mqtt = require('mqtt');
-const client = mqtt.connect('mqtt://test.mosquitto.org');
+const client = mqtt.connect('mqtt://127.0.0.1:1883', {
+    clientId: 'nodejs-mqtt',
+    clean: false
+});
 
 client.on('connect', function () {
     client.subscribe('presence');
-    client.publish('presence', 'Hello mqtt');
+    client.publish('presence', 'Hello mqtt1', {
+        retain: true,
+        qos: 1
+    });
 });
-
 client.on('message', function (topic, message) {
     // message is Buffer
     console.log(message.toString());
-    client.end();
+    //client.end();
 });
